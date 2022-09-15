45cb5cdca3434179a2bd7fbf343bf15925421421
jheisonbejarano5 <jheisonbejarano5@live.com>
2022-09-14 22:20:37 -0500

    2do homework.js

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -7,12 +7,16 @@
 function devolverString(str) {
     // "Return" la string provista: str
     // Tu código:
+    var  str
+    return str;
   }
   
   function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
+    var suma =x+y;
+    return suma;
   }
   
   function resta(x, y) {
