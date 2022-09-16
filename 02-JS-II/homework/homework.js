d024d4068899fe2619013b458f62176c631d07e5
jheisonbejarano5 <jheisonbejarano5@live.com>
2022-09-15 20:50:38 -0500

    verificacion test 2

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -77,7 +77,7 @@ function devolverString(str) {
     // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
     // De lo contrario, devuelve "false"
     // Tu código:
-    if (num > 50) {
+    if ( num > 50 ) {
       return true;
     } else {
         return false;
