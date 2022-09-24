6ed9e5abd97bb86206312aa8bda47c05d111ea38
jheisonbejarano5 <jheisonbejarano5@live.com>
2022-09-23 19:10:49 -0500

    Taller 4

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -11,7 +11,7 @@ function imprimirSumaNumeros() {
     //var numeros= [1,2,3,4,5,6,7,8,9,10];
     suma = 0;
     for(var i = 0; i <= 10; i++){
-      suma += i;
+      suma = suma + i;
     }
     return suma;
  
